e5f482c6517972e440bf1bec20bd9ff05d23b744
laercio.nsilva <lndsilva@hotmail.com>
2022-02-11 13:10:11 -0300

    cardview-layout

---
+++ app/src/main/java/br/com/local/casadasflores/Pessoa.java
@@ -0,0 +1,43 @@
+package br.com.local.casadasflores;
+
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Pessoa() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
